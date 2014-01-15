93b5f88608e3608386fbe30c7491680361fb8694
Michal Hoftich <michal.h21@gmail.com>
2014-01-15 17:49:53 +0100

    Separated functionality from script

--- iec2utf.lua
+++ iec2utf.lua
@@ -7,25 +7,8 @@ Usage: cat filename | texlua iec2utf.lua > newfilename
 Parameters:
   comma separated list of LaTeX font encodings used in the document
 ]]
-
+local iec = require "ieclib"
 local enc = {}
-
-local licrs = {}
-local codepoint2utf = unicode.utf8.char 
-function load_encodings(f)
-  local file= io.open(f,"r")
-  local encodings = file:read("*all")
-  file:close()
-  for codepoint, licr in encodings:gmatch('DeclareUnicodeCharacter(%b{})(%b{})') do
-    local codepoint = codepoint2utf(tonumber(codepoint:sub(2,-2),16))
-    local licr= licr:sub(2,-2):gsub('@tabacckludge','')
-    licrs[licr] = codepoint
-  end
-end
-
-function sanitize_licr(l)
-  return l:gsub(" (.)",function(s) if s:match("[%a]") then return " "..s else return s end end):sub(2,-2)
-end
 if arg[1] == nil then
   enc = {"T1"}
 else
@@ -34,23 +17,9 @@ else
   end
 end
 
-for _,e in pairs(enc) do
-  local filename = e:lower() .. "enc.dfu"
-  local dfufile = kpse.find_file(filename)
-  if dfufile then
-    load_encodings(dfufile)
-  end
-end
+iec.load_enc(enc)
 
 local input = io.read("*all")
-
-local cache = {}
-
-local output = input:gsub('\\IeC[%s]*(%b{})',function(iec)
-  local code = cache[iec] or licrs[sanitize_licr(iec)] or '\\IeC '..iec
-  -- print(iec, code)
-  cache[iec] = code
-  return code
-end)
+local output = iec.process(input)
 
 io.write(output)

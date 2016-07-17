1e7f2fde0fbada1770e5fef90549742b8436bf91
Michal Hoftich <michal.h21@gmail.com>
2016-07-17 21:04:47 +0200

    Reindent the lines

--- utftexindy.lua
+++ utftexindy.lua
@@ -19,43 +19,43 @@ if #arg > 0 then
 
   for i = 1,#arg do
     -- enc[#enc+1] = n
-		local n = arg[i]
-		if n == "-L" then
-			lang = arg[i+1]
-                        arg[i] = "" 
-                        arg[i + 1] ="" 
-		elseif n == "-i" then
-			print("Input")
-			input = io.read("*all")
-		elseif n == "-o" then
-			outputfile = arg[i+1]
-		end
+    local n = arg[i]
+    if n == "-L" then
+      lang = arg[i+1]
+      arg[i] = "" 
+      arg[i + 1] ="" 
+    elseif n == "-i" then
+      print("Input")
+      input = io.read("*all")
+    elseif n == "-o" then
+      outputfile = arg[i+1]
+    end
   end
 end
 
 if not input and #arg > 0 then 
-	local filename = arg[#arg]
-	local f = io.open(filename,"r")
-	if not f then 
-		print("Cannot load file: "..filename)
-		os.exit(-1)
-	end
-	input = f:read("*all")
-	f:close()
-	arg[#arg]=nil
-	if not outputfile then
-		outputname = filename:gsub("%.[^%.]-$",".ind")
-	end
+  local filename = arg[#arg]
+  local f = io.open(filename,"r")
+  if not f then 
+    print("Cannot load file: "..filename)
+    os.exit(-1)
+  end
+  input = f:read("*all")
+  f:close()
+  arg[#arg]=nil
+  if not outputfile then
+    outputname = filename:gsub("%.[^%.]-$",".ind")
+  end
 elseif #arg == 0 then
-		print("Error: You must specify filename or use -i option for standard input")
-		os.exit(1)
+  print("Error: You must specify filename or use -i option for standard input")
+  os.exit(1)
 end
 
 iec.load_enc(enc)
 
 if not outputfile and not outputname then
-	print("No output name")
-	os.exit(-2)
+  print("No output name")
+  os.exit(-2)
 end
 
 -- local input = io.read("*all")
@@ -63,12 +63,12 @@ local output = iec.process(input)
 
 local xindyopt = {"-i", "-M", "lang/"..lang.."/utf8-lang"}
 for _, o in ipairs(arg) do
-	table.insert(xindyopt,o)
+  table.insert(xindyopt,o)
 end
 
 if outputname then 
-	table.insert(xindyopt,"-o")
-	table.insert(xindyopt,outputname)
+  table.insert(xindyopt,"-o")
+  table.insert(xindyopt,outputname)
 end
 
 -- io.write(output)

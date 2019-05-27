c18a5176452e8423cfb6635d84fc06e38723fb1e
Michal Hoftich <michal.h21@gmail.com>
2019-05-27 13:09:06 +0200

    Clear \protect commands

--- ieclib.lua
+++ ieclib.lua
@@ -34,6 +34,9 @@ local cache = {}
 
 local process = function(input)
 	local output = input:gsub('\\IeC[%s]*(%b{})',function(iec)
+    -- remove \protect commands 
+    local iec = iec:gsub("\\protect%s*", "")
+    print(iec)
 		local code = cache[iec] or licrs[sanitize_licr(iec)] or '\\IeC '..iec
 		-- print(iec, code)
 		cache[iec] = code
